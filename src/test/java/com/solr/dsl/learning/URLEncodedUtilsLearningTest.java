e573913e27017bfec9285a620648f7a6a70abdc3
Guilherme H. Bueno <guilhermehbueno@gmail.com>
2017-04-20 09:46:05 -0300

    Ajustado teste.

--- src/test/java/com/solr/dsl/learning/URLEncodedUtilsLearningTest.java
+++ src/test/java/com/solr/dsl/learning/URLEncodedUtilsLearningTest.java
@@ -13,8 +13,8 @@ public class URLEncodedUtilsLearningTest {
     
     @Test
     public void test(){
-	List<NameValuePair> pairs = URLEncodedUtils.parse("fq=Marca_ClassMaster_string:Black\\ \\%26\\ Decker", Charset.defaultCharset());
-	Assert.assertEquals(pairs.size(), 1);
+	List<NameValuePair> pairs = URLEncodedUtils.parse("q=*%3A*&fq=%7B%21ex%3Dcor%7DMarca_ClassMaster_string%3ABlack+%26+Decker&facet.field=%7B%21ex%3Dcor%7Dspec_fct_Cor", Charset.defaultCharset());
+	Assert.assertEquals(pairs.size(), 3);
 	
 //	List<NameValuePair> pairs = URLEncodedUtils.parse("fq=Marca_ClassMaster_string:Black\\ \\&\\ Decker", Charset.defaultCharset());
 //	Assert.assertEquals(pairs.size(), 1);
